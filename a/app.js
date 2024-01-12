341612c8f73fd02e9e365b77beef5965eb548851
AmarMuric04 <113850369+AmarMuric04@users.noreply.github.com>
2024-01-12 08:38:03 +0100

    feat / style: added Facts page and features to go along with it

--- a/app.js
+++ a/app.js
@@ -1,5 +1,7 @@
 const randomJoke = document.querySelector(".random-joke");
 const generateBtn = document.querySelector(".generate");
+const joke = document.querySelector(".joke");
+const input = document.querySelector(".input");
 
 const ranJoke = async function () {
   const response = await fetch("https://api.chucknorris.io/jokes/random");
@@ -17,4 +19,73 @@ generateBtn.addEventListener("click", function () {
     ranJoke();
     generateBtn.innerHTML = "GENERATE NEW";
   }, 500);
+});
+
+document.querySelectorAll(".category").forEach((el) =>
+  el.addEventListener("click", async () => {
+    const response = await fetch(
+      `https://api.chucknorris.io/jokes/random?category=${el.textContent.toLowerCase()}`
+    );
+    const data = await response.json();
+    console.log(data, el.textContent);
+    document.querySelector(
+      ".joke-container"
+    ).innerHTML = `  <div class="joke-container">
+        <p class="information">
+          Click on a category or search up a word to get a fact.
+        </p>
+        <div class="show-joke">
+          <p class="joke">${data.value}</p>
+          <p class="category-show">${el.textContent}</p>
+        </div>`;
+  })
+);
+
+const getJokeBySearch = async function (input) {
+  const response = await fetch(
+    `https://api.chucknorris.io/jokes/search?query=${input}`
+  );
+  const data = await response.json();
+
+  console.log(data);
+
+  document.querySelector(".joke-container").innerHTML = `
+        <div class="card-container">
+          <p class="information">
+            Click on a category or search up a word to get a fact.
+          </p>
+          <div class="card">
+            <p>
+          ${data.result[0].value}
+            </p>
+          </div>
+          <div class="card">
+            <p>
+          ${data.result[1].value}
+           
+            </p>
+          </div>
+          <div class="card">
+            <p>         
+           ${data.result[2].value}
+            </p>
+          </div>
+          <div class="card">
+            <p>
+             ${data.result[3].value}
+            </p>
+          </div>
+          <div class="card">
+            <p>
+             ${data.result[4].value}
+            </p>
+          </div>
+        </div>`;
+};
+
+document.querySelector(".form").addEventListener("submit", async function (e) {
+  e.preventDefault();
+  if (!input) return;
+
+  getJokeBySearch(input.value);
 });
