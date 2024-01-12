2368462993123a3946681f7cf0e673759be78f7f
AmarMuric04 <113850369+AmarMuric04@users.noreply.github.com>
2024-01-12 12:01:49 +0100

    feat / style: Facts page completed

--- app.js
+++ app.js
@@ -1,98 +1,146 @@
-const input = document.querySelector("input");
-const gen = document.querySelector(".generate");
-const form = document.querySelector(".form");
-const buttons = document.querySelector(".button-container");
 const randomJoke = document.querySelector(".random-joke");
-const newRan = document.querySelector(".new-random");
+const generateBtn = document.querySelector(".generate");
+const joke = document.querySelector(".joke");
+const input = document.querySelector(".input");
 
-const chuckNorris = async function (input) {
+let value = "";
+
+const ranJoke = async function () {
+  const response = await fetch("https://api.chucknorris.io/jokes/random");
+  const data = await response.json();
+
+  randomJoke.innerHTML = '"' + data.value + '"';
+};
+
+ranJoke();
+
+generateBtn.addEventListener("click", function () {
+  generateBtn.innerHTML = `<img src="load.png" class="load-icon" />`;
+
+  setTimeout(() => {
+    ranJoke();
+    generateBtn.innerHTML = "GENERATE NEW";
+  }, 500);
+});
+
+document.querySelectorAll(".category").forEach((el) =>
+  el.addEventListener("click", async () => {
+    const response = await fetch(
+      `https://api.chucknorris.io/jokes/random?category=${el.textContent.toLowerCase()}`
+    );
+    const data = await response.json();
+    document.querySelector(
+      ".joke-container"
+    ).innerHTML = `  <div class="joke-container">
+        <div class="show-joke">
+          <p class="joke">${data.value}</p>
+          <p class="category-show">${el.textContent}</p>
+        </div>`;
+  })
+);
+
+const getJokeBySearch = async function (input, page = 0) {
   const response = await fetch(
     `https://api.chucknorris.io/jokes/search?query=${input}`
   );
   const data = await response.json();
 
-  document.querySelector(".joke-container").innerHTML = `  
-   <span class="random-joke-container"
-        ><img src="screw.png" /><span class="random-joke">${input}</span
-        ><img src="screw.png"
-      /></span>`;
-  document.querySelector(".joke-container").innerHTML += data.result
-    .map((el) => `<p>${el.value}</p>`)
-    .join("\n");
-};
+  console.log(data);
 
-form.addEventListener("submit", async (e) => {
-  e.preventDefault();
-  if (!input.value) return;
+  const numPages = Math.ceil(data.result.length / 5);
+  const start = page * 5;
+  const end = start + 5;
+  let curPage = +page;
 
-  await chuckNorris(input.value);
-  input.value = "";
-});
+  document.querySelector(".joke-container").innerHTML = `
+       <div class="card-container"> </div>`;
 
-document.querySelectorAll(".category").forEach((el) =>
-  el.addEventListener("click", async function () {
-    document.querySelector(
-      ".joke-container"
-    ).innerHTML = `    <span class="random-joke-container" id="${
-      el.textContent
-    }">
-      <img src="screw.png" />
-      <span class="random-joke">
-        ${el.textContent[0].toUpperCase() + el.textContent.slice(1)}
-      </span>
-      <img src="screw.png" />
-    </span>`;
-
-    for (let i = 0; i < 10; i++) {
-      const response = await fetch(
-        `https://api.chucknorris.io/jokes/random?category=${el.textContent}`
-      );
-      const data = await response.json();
-
-      if (
-        document
-          .querySelector(".joke-container")
-          .innerHTML.includes(`${data.value}`)
-      )
-        continue;
-
-      document.querySelector(
-        ".joke-container"
-      ).innerHTML += `<p>${data.value}</p>`;
-    }
-  })
-);
-
-window.addEventListener("scroll", function () {
-  const nav = document.querySelector("nav");
-
-  const navBounding = nav.getBoundingClientRect();
-  if (navBounding.bottom <= 50) {
-    buttons.classList.add("fixed");
-  } else {
-    buttons.classList.remove("fixed");
+  for (let i = start; i < end; i++) {
+    if (!data.result[i]) break;
+    document.querySelector(".card-container").innerHTML += `  <div class="card">
+            <p>
+         ${data.result[i].value}
+            </p>
+          </div>`;
   }
+
+  document.querySelector(".joke-container").innerHTML += `
+       <div class="pagination"> </div>`;
+
+  //first page with more pages
+  if (curPage + 1 === 1 && numPages > 1) {
+    document.querySelector(".pagination").innerHTML = `   
+    <p class="pages"> <span>page</span> ${curPage + 1} of ${numPages} </p>
+      <button class="go-right" data-page="${curPage + 1}">
+        Page ${curPage + 2} <i class="fa-solid fa-arrow-right"></i>
+      </button>
+    </div>
+`;
+    const rightPag = document.querySelector(".go-right");
+
+    rightPag.addEventListener("click", function () {
+      const nextPage = rightPag.dataset.page;
+      getJokeBySearch(value, nextPage);
+    });
+  }
+  //other page
+  else if (
+    curPage + 1 !== 1 &&
+    curPage + 1 !== numPages &&
+    numPages > curPage
+  ) {
+    document.querySelector(".pagination").innerHTML += `   
+        <button class="go-left" data-page="${curPage - 1}">
+      <i class="fa-solid fa-arrow-left"></i> Page ${curPage} 
+        </button>
+       <p class="pages"> <span>page</span> ${curPage + 1} of ${numPages} </p>
+        <button class="go-right" data-page="${curPage + 1}">
+          Page ${curPage + 2} <i class="fa-solid fa-arrow-right"></i>
+        </button>
+      </div>
+  `;
+
+    const rightPag = document.querySelector(".go-right");
+
+    rightPag.addEventListener("click", function () {
+      const nextPage = rightPag.dataset.page;
+      getJokeBySearch(value, nextPage);
+    });
+    const leftPag = document.querySelector(".go-left");
+    leftPag.addEventListener("click", function () {
+      const prevPage = leftPag.dataset.page;
+      getJokeBySearch(value, prevPage);
+    });
+  } else if (curPage + 1 === numPages && numPages !== 0) {
+    document.querySelector(".pagination").innerHTML += ` 
+        <button class="go-left" data-page="${curPage - 1}">
+        <i class="fa-solid fa-arrow-left"></i>  Page ${curPage} 
+        </button>
+        <p class="pages"> <span>page</span> ${curPage + 1} of ${numPages} </p>
+        `;
+    const leftPag = document.querySelector(".go-left");
+    leftPag.addEventListener("click", function () {
+      const prevPage = leftPag.dataset.page;
+      getJokeBySearch(value, prevPage);
+    });
+  }
+  //first page with no other pages
+  else
+    document.querySelector(
+      ".pagination"
+    ).innerHTML = `<p class="pages"> <span>page</span> ${
+      curPage + 1
+    } of ${numPages} </p>`;
+};
+
+document.querySelector(".form").addEventListener("submit", async function (e) {
+  e.preventDefault();
+  if (!input) return;
+  value = input.value;
+
+  getJokeBySearch(input.value);
 });
-let ranNum;
-const getRandomJoke = async function () {
-  ranNum = Math.random() * 1;
-  randomJoke.innerHTML = `<img class="rotate" src="image.png" />`;
-  const ranRes = await fetch("https://api.chucknorris.io/jokes/random");
-  const ranData = await ranRes.json();
-  setTimeout(async () => {
-    randomJoke.textContent = ranData.value;
-  }, ranNum * 1000);
-};
-getRandomJoke();
 
-const getSpinner = function () {
-  newRan.innerHTML = `<img class="rotate" src="reload.png" />`;
-};
-
-newRan.addEventListener("click", async function () {
-  getSpinner();
-  await getRandomJoke();
-  setTimeout(() => {
-    newRan.innerHTML = "NEW";
-  }, ranNum * 1000);
+document.querySelector(".category-button").addEventListener("click", (e) => {
+  document.querySelector(".category-container").classList.toggle("hidden");
 });
